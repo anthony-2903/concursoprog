c1f268f3cbac8425cdb8e2f4c773cd6338a11b03
anthony-2903 <anthony.janampa@upeu.edu.pe>
2025-09-28 21:16:44 -0500

    .

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,5 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    if (process.env.NODE_ENV === "development") {
-      return []; // 🔹 No aplicar headers en desarrollo
-    }
-
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; img-src 'self' data: https:; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
